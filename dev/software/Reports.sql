26fb8f86d485588844c921b90e86c9d14c746eb0
Craig McKay <craig@goodteaching.org>
2015-12-06 17:34:36 +0000

    #131978
    First version created.

--- dev/software/Reports.sql
+++ dev/software/Reports.sql
@@ -1,11 +1,11 @@
-SELECT br.bookid, bb.bookname, br.chapter, br.author, br.vol, br.page, a.article, a.scriptures
+SELECT br.bookid, bb.bookname, br.chapter, br.author, br.vol, br.page, a.article, MIN(a.scriptures) scriptures
 FROM mse_bible_ref br, mse_bible_book bb, mse_article a
 WHERE bb.bookid = br.bookid
 AND a.author = br.author
 AND a.vol = br.vol
 AND a.page = br.article_page
 AND br.article_primary=1
-GROUP BY br.bookid, bb.bookname, br.chapter, br.author, br.vol, br.page, a.article, a.scriptures
+GROUP BY br.bookid, bb.bookname, br.chapter, br.author, br.vol, br.page, a.article
 ORDER BY 1,3,4,5,6;
 
 
@@ -23,4 +23,9 @@ FROM (
 GROUP BY bookid, chapter
 ORDER BY 1,3;
 
-
+-- Find invalid chapter references
+SELECT bb.bookname, br . *
+FROM mse_bible_ref br
+INNER JOIN mse_bible_book bb ON bb.bookid = br.bookid
+WHERE br.chapter > bb.no_of_chapters
+LIMIT 0 , 30
