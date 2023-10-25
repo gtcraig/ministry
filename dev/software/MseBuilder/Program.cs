97943124af8da942bd04961b1f7b5fdd564c1c21
gtcraig <craig@goodteaching.org>
2023-10-25 21:11:51 +0100

    Ensure more modern presentation of Window.

--- dev/software/MseBuilder/Program.cs
+++ dev/software/MseBuilder/Program.cs
@@ -22,7 +22,7 @@ namespace FrontBurner.Ministry.MseBuilder
     [STAThread]
     static void Main()
     {
-      Application.EnableVisualStyles();
+      //Application.EnableVisualStyles();
       Application.SetCompatibleTextRenderingDefault(false);
       Application.Run(new MseBuilder());
     }
